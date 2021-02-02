0f6a06f9bf8647dcf9494472860f62fe137c3229
3xEm3 <65020395+3xEm3@users.noreply.github.com>
2021-02-02 17:17:55 +0300

    Update gullery.php

--- lesson_4/task_2/gullery.php
+++ lesson_4/task_2/gullery.php
@@ -7,7 +7,7 @@ $images = scandir($dir);
 foreach ($images as $image){
 
     if($image != '.' && $image != '..' && $image != 'thumb') {
-		echo '<a class="gul-img" href="'.$dir.$image.'" target="_blank"><img src="'.$dir_thumb.'thumb_'.$image.'"/></a>';
+		echo '<a class="gul-img" href="' . $dir . $image . '" target="_blank"><img src="' . $dir_thumb . 'thumb_' . $image . '"/></a>';
 	}
 
 }
